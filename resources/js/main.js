11f61494d5f98a49d04d8c58075099a954fe5589
lightfootc <cody.c.lightfoot@gmail.com>
2018-03-05 16:16:57 -0600

    Finished with the header and hero content. Just need to add a picture of self.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // This adds the background color to the navbar on scroll
     $(window).scroll(function(){
         if ($(window).scrollTop()){
             $('header').addClass('navBackground');
@@ -7,8 +8,15 @@ $(document).ready(function(){
             $('header').removeClass('navBackground');
         }
     });
-    
+    //This toggles the hamburger icon for the mobile nav
     $('.mobile-nav').click(function(){
         $('.navbar ul').toggleClass('active');
     });
+    // This adds a blur effect to images on scroll
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        $('.image-blur').css({
+            filter: 'blur(' + (scroll/80) + 'px)'
+        })
+    })
 });
